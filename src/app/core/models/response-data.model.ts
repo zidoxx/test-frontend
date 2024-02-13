51988136e1dd47eba13109963e9b15747ca39974
Esteban Castaño Rivera <estebancastriver@gmail.com>
2024-02-13 15:10:41 -0500

    Add search algorithm to find the flight paths, add response data model to map the api response, implement the service in the search component

---
+++ src/app/core/models/response-data.model.ts
@@ -0,0 +1,7 @@
+export interface ResponseData {
+  departureStation: string;
+  arrivalStation: string;
+  flightCarrier: string;
+  flightNumber: string;
+  price: number;
+}
---
+++ src/app/services/flight.service.ts
@@ -0,0 +1,101 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Journey } from '../core/models/journey.model';
+import { ResponseData } from '../core/models/response-data.model';
+@Injectable({
+  providedIn: 'root',
+})
+export class FlightService {
+  private flightsUrl = 'https://recruiting-api.newshore.es/api/flights/2';
+  private errorMessage = 'No se pudieron cargar los vuelos';
+  private pathErrorMessage = 'No se puede calcular la ruta.';
+  public http = inject(HttpClient);
+
+  private getFlights(): Observable<ResponseData[]> {
+    return this.http.get<ResponseData[]>(this.flightsUrl);
+  }
+
+  public findRoute(origin: string, destination: string): Observable<Journey> {
+    return new Observable((observer) => {
+      this.getFlights().subscribe(
+        (flights) => {
+          const route = this.findRouteHelper(flights, origin, destination);
+          if (route) {
+            // Construir el objeto en el modelo entregado
+            const journey: Journey = {
+              origin: route[0].departureStation,
+              destination: route[route.length - 1].arrivalStation,
+              price: route.reduce((acc, flight) => acc + flight.price, 0),
+              flights: route.map((flight: ResponseData) => ({
+                origin: flight.departureStation,
+                destination: flight.arrivalStation,
+                price: flight.price,
+                transport: {
+                  flightCarrier: flight.flightCarrier,
+                  flightNumber: flight.flightNumber,
+                },
+              })),
+            };
+            observer.next(journey);
+          } else {
+            observer.error(this.pathErrorMessage);
+          }
+        },
+        (error) => {
+          observer.error(this.errorMessage);
+        }
+      );
+    });
+  }
+
+  private findRouteHelper(
+    flights: ResponseData[],
+    origin: string,
+    destination: string
+  ): ResponseData[] | undefined {
+    function findAllRoutes(
+      origin: string,
+      currentRoute: any[],
+      visited: Set<string>
+    ): any[] {
+      if (origin === destination) {
+        return [currentRoute];
+      }
+
+      //Evitar repetidos en la ruta
+      visited.add(origin);
+
+      const possibleFlights = flights.filter(
+        (flight) =>
+          flight.departureStation === origin &&
+          !visited.has(flight.arrivalStation)
+      );
+
+      let allRoutes: any[] = [];
+
+      for (const flight of possibleFlights) {
+        const nextRoute = [...currentRoute, flight];
+        const nextVisited = new Set(visited);
+        //Se utiliza recursividad para encontrar todas las rutas
+        allRoutes = [
+          ...allRoutes,
+          ...findAllRoutes(flight.arrivalStation, nextRoute, nextVisited),
+        ];
+      }
+
+      return allRoutes;
+    }
+
+    // Encuentra todas las rutas posibles
+    const allRoutes = findAllRoutes(origin, [], new Set());
+
+    // Encuentra la primera ruta encontrada
+    if (allRoutes.length > 0) {
+      return allRoutes[0];
+    }
+    {
+      return undefined;
+    }
+  }
+}
